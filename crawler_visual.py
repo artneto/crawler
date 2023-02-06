cc9f259b36734b105b2bf77a498db48195630adc
artneto <arthur_neto@icloud.com>
2023-02-06 09:47:17 +0000

    create objects for the pages

--- crawler_visual.py
+++ crawler_visual.py
@@ -2,4 +2,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-soup = BeautifulSoup(html_doc, 'html.pearse')
+url = 'https://www.myhome.ie/rentals/ireland/property-to-rent'
+page = requests.get(url)
+
+# This object will have two paraments to get the html and to parse the html.
+soup = BeautifulSoup(page.content, 'html.parser')
+lists = soup.find_all('div', class_='PropertyListingCard__Content')
+
+# loop to check the same information in other pages
+
+for list in lists:
+    title = list.find('div', class_='PropertyListingCard__Address ng-tns-c193-1')
+    price = list.find('div', class_='PropertyListingCard__Price ng-tns-c193-1')
+    info = [title,price]
+    print(info)
