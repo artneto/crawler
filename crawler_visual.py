d0d2ba7228ec262d2e4cab886c6696bc72dd063b
artneto <arthur_neto@icloud.com>
2023-02-08 14:41:04 +0000

    add loop for scrappy multiples pages

--- crawler_visual.py
+++ crawler_visual.py
@@ -3,26 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-url = 'https://www.myhome.ie/rentals/ireland/property-to-rent'
-page = requests.get(url)
+
+page = 1
+while page != 25:
+      url = f"https://www.myhome.ie/rentals/ireland/property-to-rent?page={page}"
+      print(url)
+      page = page + 1
+    
+response  = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 lists = soup.find_all('div', class_='PropertyListingCard__Content')
+print(lists)
 
-
+'''
 # Open the file for writing
-with open('housefile.csv', 'w', encoding='utf8', newline='') as f:
-    thewriter = writer(f)
-    header = ['Title', 'Price', 'Room']
-    thewriter.writerow(header)
+    with open('housefile.csv', 'w', encoding='utf8', newline='') as f:
+        thewriter = writer(f)
+        header = ['Title', 'Price', 'Room']
+        thewriter.writerow(header)
 
-  
+
 
     # Loop over the property listings
     for list in lists:
         title = list.find('a', class_='PropertyListingCard__Address').text
         price = list.find('div', class_='PropertyListingCard__Price').text
         room = list.find('span', class_='PropertyInfoStrip__Detail').text
- 
+
         # Write the data to the file
         info = [title, price, room]
-        thewriter.writerow(info)
+        thewriter.writerow(info)'''
