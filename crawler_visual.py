0221dc1c154871559e4e48ae881c7418e23c5f81
artneto <arthur_neto@icloud.com>
2023-02-12 19:01:29 +0000

    create try except block for property list

--- crawler_visual.py
+++ crawler_visual.py
@@ -3,29 +3,58 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
-# Make a request to the URL
-url = "https://www.myhome.ie/rentals/ireland/property-to-rent"
-response  = requests.get(url)
 
-# Parse the HTML
-soup = BeautifulSoup(response.content, 'html.parser')
-lists = soup.find_all('div', class_='PropertyListingCard__Content')
+base_url = "https://www.myhome.ie/rentals/ireland/property-to-rent?"
 
-# Open the file for writing
+
+property_list = []
+
+
+for i in range(1, 25):    
+    url=base_url+f"page={i}"
+    print(url)
+    response  = requests.get(url)
+
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    lists = soup.find_all('div', class_='PropertyListingCard__Content')
+
+
+    # Loop over the property listings
+    for list in lists:
+        try:
+            title = list.find('a', class_='PropertyListingCard__Address').text
+        except AttributeError:
+            title = "none"
+        try: 
+            price = list.find('div', class_='PropertyListingCard__Price').text
+        except AttributeError:
+            price = "none"
+        try:
+            room = list.find('span', class_='PropertyInfoStrip__Detail').text
+        except AttributeError:
+            room = "none"
+
+
+        # Write the data to the file
+        info = [title, price, room]
+        property_list.append(info)
+
+
+    print(f'page {i} Done _______________')
+    print(len(lists))
+    
+#Creating csv file and adding data to it
 f = open('housefile.csv', 'w', encoding='utf8', newline='')
 thewriter = writer(f)
 header = ['Title', 'Price', 'Room']
 thewriter.writerow(header)
 
-# Loop over the property listings
-for list in lists:
-    title = list.find('a', class_='PropertyListingCard__Address').text
-    price = list.find('div', class_='PropertyListingCard__Price').text
-    room = list.find('span', class_='PropertyInfoStrip__Detail').text
 
-    # Write the data to the file
-    info = [title, price, room]
-    thewriter.writerow(info)
-    print(info)
+
+
+
+for property in property_list:
+    thewriter.writerow(property)
 f.close()
-print("done")
+print("Completed")
